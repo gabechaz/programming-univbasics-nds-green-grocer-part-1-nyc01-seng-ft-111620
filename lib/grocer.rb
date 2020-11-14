6618f641cc3a635ccb6b6fb225779b6327014b0a
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-14 17:53:09 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,23 +1,7 @@
 require 'pry'
-<<<<<<< HEAD
-
-def find_item_by_name_in_collection(name, collection)
-  return_value = nil
-collection.each do |item|
-  item.each do |key, value|
-    if value == name
-      return_value = item
-    end
-  end
-end
-return_value
-end
- 
- 
-  
 
 
-=======
+
 
 def find_item_by_name_in_collection(name, collection)
   return_value = nil
@@ -30,31 +14,18 @@ def find_item_by_name_in_collection(name, collection)
  end
  return_value
  end
- 
- 
-   
-
-
->>>>>>> 91fb4d71c07c6582e7f6140f8552dcda2f825082
 
 def consolidate_cart(cart)
   consolidated_cart = []
   cart.each do |item|
-<<<<<<< HEAD
+
     if consolidated_cart.include?(item)
       item[:count] = item[:count] + 1
-=======
-    
     if consolidated_cart[item]
-      
-
     else 
       consolidated_cart << item
       item[:count] = 1
     end
-
-      
-
     end
   end
 
